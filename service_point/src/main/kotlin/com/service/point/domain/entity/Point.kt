20cb5d44b504d9b3b65070d37b093013aa435f12
gkak12 <hyunjun5681@kakao.com>
2025-09-06 19:38:35 +0900

    포인트 엔티티 추가

---
+++ service_point/src/main/kotlin/com/service/point/domain/entity/Point.kt
@@ -0,0 +1,35 @@
+package com.service.point.domain.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "entity")
+class Point (
+
+    @Id
+    @Column(name = "POINT_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var pointSeq: Long = 0,
+
+    @Column(name = "USER_SEQ")
+    var userSeq: Long = 0,
+
+    @Column(name = "TYPE_SEQ")
+    var typeSeq: Long = 0,
+
+    @Column(name = "ORDER_SEQ")
+    var orderSeq: Long = 0,
+
+    @Column(name = "AMOUNT")
+    var amount: Long = 0,
+
+    @Column(name = "CONTENT")
+    var content: String = "",
+
+    @Column(name = "CREATION_DATE_TIME")
+    var creationDateTime: LocalDateTime? = null,
+
+    @Column(name = "EXPIRATION_DATE_TIME")
+    var expirationDateTime: LocalDateTime? = null
+)
